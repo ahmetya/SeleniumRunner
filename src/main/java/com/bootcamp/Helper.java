f9fd62ba975e6632d3f8d0d26369fe3ab094506f
Ahmet Yalcinkaya <ahmetyky@gmail.com>
2022-08-11 20:41:35 +0200

    Selenium TestNG JUnit

--- src/main/java/com/bootcamp/Helper.java
+++ src/main/java/com/bootcamp/Helper.java
@@ -3,12 +3,15 @@ package com.bootcamp;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 
 public class Helper {
-    public static void starter() throws InterruptedException {
+    static void starter() throws InterruptedException {
+    }
+    public void callTest() throws InterruptedException {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--verbose", "--headless", "--disable-web-security", "--ignore-certificate-errors", "--allow-running-insecure-content", "--allow-insecure-localhost", "--no-sandbox", "--disable-gpu");
@@ -19,7 +22,6 @@ public class Helper {
         WebElement rejectButton = driver.findElement(By.cssSelector("#W0wltc > div"));
         WebElement searchField = driver.findElement(By.name("q"));
         WebElement searchButton = driver.findElement(By.cssSelector("div[class=\"FPdoLc lJ9FBc\"] > center > input[value=\"Google Search\"][role=\"button\"] "));
-
 
 
         rejectButton.click();
@@ -91,7 +93,7 @@ public class Helper {
         driver.manage().window().maximize();
         driver.manage().window().minimize();
 
-        Dimension dimension = new Dimension(1200,200);
+        Dimension dimension = new Dimension(1200, 200);
         driver.manage().window().setSize(dimension);
 
         Point point = driver.manage().window().getPosition();
@@ -121,12 +123,100 @@ public class Helper {
         Boolean selectStatus = userInputFields.get(1).isSelected();
         System.out.println(enableStatus + "  ###################");
 
-
-
-
-
         Thread.sleep(5000);
     }
+    public void createPatient() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        //go to browser.
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+
+        // WebElement Find
+
+        String username = "Admin";
+        String password = "Admin123";
+
+        // LOCATOR DEFINITIONS - LOGIN PAGE
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        List<WebElement> locations = driver.findElements(By.cssSelector("ul[id='sessionLocation'] li"));
+        WebElement loginButton = driver.findElement(By.cssSelector("input[id='loginButton']"));
+
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        locations.get(0).click();
+        loginButton.click();
+
+        // PAGE OPERATIONS - DASHBOARD
+        WebElement registerPatientButton = driver.findElement(By.partialLinkText("Register a patient"));
+
+        registerPatientButton.click();
+
+        // PAGE OPERATIONS - REGISTER PATIENT
+        WebElement patientNameInput = driver.findElement(By.name("givenName"));
+        WebElement patientFamilyNameInput = driver.findElement(By.name("familyName"));
+
+        patientNameInput.sendKeys("Patient");
+        patientFamilyNameInput.sendKeys("Zero");
+
+        WebElement nextButton = driver.findElement(By.id("next-button"));
+        nextButton.click();
+
+        List<WebElement> genderSelectBox = driver.findElements(By.cssSelector("select[id='gender-field'] option"));
+        genderSelectBox.get(0).click();
+        nextButton.click();
+
+        WebElement birthDateDay = driver.findElement(By.id("birthdateDay-field"));
+        WebElement birthDateYear = driver.findElement(By.id("birthdateYear-field"));
+
+        Random rand = new Random();
+        int birthDay = rand.nextInt((31 - 1) + 1) + 1;
+        int year = rand.nextInt((2000 - 1900) + 1) + 1900;
+        int month = rand.nextInt((12 - 1) + 1) + 1;
+
+        WebElement birthDateMonth = driver.findElement((By.cssSelector("select[id='birthdateMonth-field'] > option[value='" + month + "']")));
+
+        birthDateDay.sendKeys("" + birthDay);
+        birthDateYear.sendKeys("" + year);
+        birthDateMonth.click();
+        nextButton.click();
+
+        WebElement postalCodeInput = driver.findElement(By.cssSelector("input[id='postalCode']"));
+        postalCodeInput.sendKeys("34000");
+        nextButton.click();
+
+        List<WebElement> okIcons = driver.findElements(By.cssSelector("i[class='icon-ok']"));
+        Boolean okIconsDisplayed = okIcons.get(1).isDisplayed();
+        System.out.println(okIconsDisplayed + "##############");
+        Assert.assertEquals(okIconsDisplayed, okIconsDisplayed);
 
 
+        WebElement phoneNumberInput = driver.findElement(By.name("phoneNumber"));
+
+        int phoneNumber = rand.nextInt((9999 - 1000) + 1) + 1000;
+        phoneNumberInput.sendKeys("+356 9978" + phoneNumber);
+        nextButton.click();
+
+        String relativeType = "Parent";
+
+        WebElement relativeSelect = driver.findElement(By.cssSelector("select[id='relationship_type'] option[data-val='" + relativeType + "']"));
+
+        WebElement relativeName = driver.findElement(By.cssSelector("input[placeholder='Person Name']"));
+        relativeName.sendKeys("Parent Zero");
+        nextButton.click();
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        Thread.sleep(5000);
+        driver.quit();
+
+        // HOMEWORK
+        /*  Register Patient ID in a variable on the top right corner of page
+            Go to main dashboard
+            Go to search patient
+            Input patient id to search box
+            Execute search
+         */
+    }
 }
--- src/main/java/com/bootcamp/Runner.java
+++ src/main/java/com/bootcamp/Runner.java
@@ -4,100 +4,17 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
-import java.util.Random;
 
 public class Runner {
     private static List<WebElement> userInputFields;
 
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        //go to browser.
-        driver.get("https://demo.openmrs.org/openmrs/login.htm");
 
-        // WebElement Find
+        Helper help = new Helper();
+        // help.callTest();
+        help.createPatient();
 
-        String username = "Admin";
-        String password = "Admin123";
 
-        // LOCATOR DEFINITIONS - LOGIN PAGE
-        WebElement usernameInput = driver.findElement(By.id("username"));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        List<WebElement> locations = driver.findElements(By.cssSelector("ul[id='sessionLocation'] li"));
-        WebElement loginButton = driver.findElement(By.cssSelector("input[id='loginButton']"));
 
-        usernameInput.sendKeys(username);
-        passwordInput.sendKeys(password);
-        locations.get(0).click();
-        loginButton.click();
-
-        // PAGE OPERATIONS - DASHBOARD
-        WebElement registerPatientButton = driver.findElement(By.partialLinkText("Register a patient"));
-
-        registerPatientButton.click();
-
-        // PAGE OPERATIONS - REGISTER PATIENT
-        WebElement patientNameInput = driver.findElement(By.name("givenName"));
-        WebElement patientFamilyNameInput = driver.findElement(By.name("familyName"));
-
-        patientNameInput.sendKeys("Patient");
-        patientFamilyNameInput.sendKeys("Zero");
-
-        WebElement nextButton = driver.findElement(By.id("next-button"));
-        nextButton.click();
-
-        List<WebElement> genderSelectBox = driver.findElements(By.cssSelector("select[id='gender-field'] option"));
-        genderSelectBox.get(0).click();
-        nextButton.click();
-
-        WebElement birthDateDay = driver.findElement(By.id("birthdateDay-field"));
-        WebElement birthDateYear = driver.findElement(By.id("birthdateYear-field"));
-
-        Random rand = new Random();
-        int birthDay = rand.nextInt((31 - 1) + 1) + 1;
-        int year = rand.nextInt((2000 - 1900) + 1) + 1900;
-        int month = rand.nextInt((12 - 1) + 1) + 1;
-
-        WebElement birthDateMonth = driver.findElement((By.cssSelector("select[id='birthdateMonth-field'] > option[value='" + month + "']")));
-
-        birthDateDay.sendKeys("" + birthDay);
-        birthDateYear.sendKeys("" + year);
-        birthDateMonth.click();
-        nextButton.click();
-
-        WebElement postalCodeInput = driver.findElement(By.cssSelector("input[id='postalCode']"));
-        postalCodeInput.sendKeys("34000");
-        nextButton.click();
-
-        List<WebElement> okIcons = driver.findElements(By.cssSelector("i[class='icon-ok']"));
-        Boolean okIconsDisplayed = okIcons.get(1).isDisplayed();
-        System.out.println(okIconsDisplayed + "##############");
-
-        WebElement phoneNumberInput = driver.findElement(By.name("phoneNumber"));
-
-        int phoneNumber = rand.nextInt((9999 - 1000) + 1) + 1000;
-        phoneNumberInput.sendKeys("+356 9978" + phoneNumber);
-        nextButton.click();
-
-        String relativeType = "Parent";
-
-        WebElement relativeSelect = driver.findElement(By.cssSelector("select[id='relationship_type'] option[data-val='" + relativeType +"']"));
-
-        WebElement relativeName = driver.findElement(By.cssSelector("input[placeholder='Person Name']"));
-        relativeName.sendKeys("Parent Zero");
-        nextButton.click();
-
-        WebElement submitButton = driver.findElement(By.id("submit"));
-        submitButton.click();
-
-        Thread.sleep(500000);
-        driver.quit();
-
-        // HOMEWORK
-        /*  Register Patient ID in a variable on the top right corner of page
-            Go to main dashboard
-            Go to search patient
-            Input patient id to search box
-            Execute search
-         */
     }
 }

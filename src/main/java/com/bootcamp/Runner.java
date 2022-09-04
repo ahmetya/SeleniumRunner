345c8ca4cf6275ac7080aa72bdccb572b4d3840e
Ahmet Yalcinkaya <ahmetyky@gmail.com>
2022-09-04 17:20:17 +0200

    Changes in pom.xml and other minor refactoring

--- src/main/java/com/bootcamp/Runner.java
+++ src/main/java/com/bootcamp/Runner.java
@@ -10,12 +10,12 @@ public class Runner {
     public static void main(String[] args) throws InterruptedException {
 
         Helper help = new Helper();
-        //help.callTest();
+        help.callTest();
+
+        // comment for this line
 
         help.createPatient();
         help.generateRandom(5);
-
-
         String[] fullName = help.generateRandomName();
         System.out.println(help.generateRandomName()[0] + " " + help.generateRandomName()[1]);
         Thread.sleep(500);
